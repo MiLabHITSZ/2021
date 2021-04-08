8830515fb2a6c6af79c1622013eba6e7edc3b41a
张立才 <20S151126@stu.hit.edu.cn>
2021-04-08 11:55:29 +0800

    2021/4/7 22：04

--- build_model.py
+++ build_model.py
@@ -9,5 +9,5 @@ def build_model():
         layers.Dense(10, activation=tf.nn.softmax),
     ])
     # optimizer = optimizers.SGD(lr=0.01, decay=lr/100, momentum=0.9)
-    optimizer = tf.keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+    optimizer = optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
     return model, optimizer
