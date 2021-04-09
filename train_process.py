045802e98668c6c04945c9b692e187c69e475266
张立才 <20S151126@stu.hit.edu.cn>
2021-04-09 13:00:21 +0800

    2021/4/7 13:00

--- train_process.py
+++ train_process.py
@@ -1,58 +1,40 @@
 from tensorflow import keras
-
 from test_process import *
 import tensorflow as tf
+from build_model import *
 
 
+# 添加与窃取数据相关联的正则项
 def linear_data_leakage(model, x_test):
-    x_test = tf.reshape(x_test, [-1, 1]) / 10
-    # matrix_mean = tf.constant(0, dtype=tf.float32)
-    # data_mean = tf.constant(0, dtype=tf.float32)
-    regular = tf.constant(0)
+    x_test = tf.reshape(x_test, [-1, 1]) / 10  # 数据缩放至0-0，1
+    regular = tf.constant(0, dtype=tf.float32)
+    # 计算正则项
     for i in range(len(model.trainable_variables)):
-        if len(model.trainable_variables[i].shape) == 2:
+        if len(model.trainable_variables[i].shape) >= 2:  # 取出空间维度大于等于2的W矩阵
+            # 计算W的参数总数
             total = int(tf.shape(model.trainable_variables[i])[0]) * int(tf.shape(model.trainable_variables[i])[1])
-            data1 = x_test[0:total]
-            data1 = tf.reshape(data1, model.trainable_variables[i].shape)
-            data1 = abs(data1 - abs(model.trainable_variables[i]) - model.trainable_variables[i + 1])
-            regular += tf.norm(data1, ord=1)/float(total)
-            # data_mean += tf.reduce_mean(data1)
-            # print(tf.reduce_mean(data1))
-            # matrix_mean += tf.reduce_mean(abs(model.trainable_variables[i]))
-            # print(tf.reduce_mean(abs(model.trainable_variables[i])))
+            data_batch = x_test[0:total]  # 取出同等数量的数据
+            data_batch = tf.reshape(data_batch, model.trainable_variables[i].shape)  # 塑造成与W相同shape的矩阵
+            # 正则化公式
+            data_batch = abs(data_batch - abs(model.trainable_variables[i]) - model.trainable_variables[i + 1])
+            regular += tf.norm(data_batch, ord=1) / float(total)  # 取绝对值
             x_test = x_test[total:]
-            # print(tf.shape(data1))
-            # print(tf.shape(x_test))
-    # print('matrix_mean:', matrix_mean)
-    # print('data_mean:', data_mean)
-    # print('ration:', data_mean / matrix_mean)
     return regular
 
 
 def train(model, optimizer, train_db, x_test, y_test):
-    # model(x_test)
-    # data_list = linear_data_leakage(model, x_test)
-    for epoch in range(100):
+    for epoch in range(5):
         loss_print = 0
         for step, (x, y) in enumerate(train_db):
-            # data_list_copy = data_list
-            # sum = tf.constant(0, dtype=tf.float32)
             with tf.GradientTape() as tape:
-                out = model(x)
-                regular = linear_data_leakage(model, x_test)
-                loss = keras.losses.categorical_crossentropy(y, out, from_logits=False)+regular
+                out = model(x, training=True)
+                # regular = linear_data_leakage(model, x_test)
+                loss = keras.losses.categorical_crossentropy(y, out, from_logits=False)
                 loss = tf.reduce_mean(loss)
-
-                # sum = tf.variant(0)
-                # for i in range(len(data_list)):
-                #     data_list_copy[i] = abs(
-                #         data_list_copy[i] - abs(model.trainable_variables[2 * i]) - model.trainable_variables[2 * i + 1])
-                #     sum += tf.norm(data_list_copy[i]) / float(
-                #                 tf.shape(data_list_copy[i])[0] * tf.shape(data_list_copy[i])[1])
-                # loss = tf.reduce_mean(keras.losses.categorical_crossentropy(y, out, from_logits=False)) + sum
                 loss_print = float(loss)
-        grads = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+            grads = tape.gradient(loss, model.trainable_variables)
+        # print(model.trainable_variables[0])
+            optimizer.apply_gradients(zip(grads, model.trainable_variables))
         acc = test(model, x_test, y_test)
         print('epoch:', epoch, 'loss:', loss_print, 'Evaluate Acc:', float(acc))
 
@@ -63,8 +45,8 @@ def train1(model, optimizer, train_db, x_test, y_test):
         loss='categorical_crossentropy',
         metrics=['accuracy']
     )
-    model.fit(train_db, epochs=6, batch_size=128)
+    for i in range(5):
+        model.fit(train_db, epochs=1, batch_size=128)
+        print(model.get_weights()[0])
     y_test = tf.one_hot(y_test, depth=10)
     loss, acc = model.evaluate(x_test, y_test)
-    print('loss', loss)
-    print('acc', acc)
